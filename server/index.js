a8e5416eb22db791eff892583e52e759c1f8330e
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-20 22:35:12 -0600

    .env stuff

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
+require ('dotenv').config()
 const express = require('express'),
 const session = require('express-session'),
 const massive = require('massive')
 ctrl = require('./controller')
 
 const app = express()
+const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    saveUnitialized: false,
+    resave: false,
+    cookie: {
+        maxage: 1000 * 60 * 60
+    }
+}))
 
 massive(CONNECTION_STRING).then((database) => {
     app.set('db', database)
